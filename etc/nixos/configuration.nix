05043be94a3d21a3700bbf21a77c9901cbf03e43
Pham Duc Binh <binh1298@gmail.com>
2024-07-25 00:13:55 +0700

    feature: fail2ban

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -104,10 +104,38 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
   services.openssh.settings.PermitRootLogin = "yes";
+  services.fail2ban = {
+    enable = true;
+    # Ban IP after 5 failures
+    maxretry = 5;
+    ignoreIP = [];
+    bantime = "24h"; # Ban IPs for one day on the first ban
+    bantime-increment = {
+      enable = true; # Enable increment of bantime after each violation
+      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
+      multipliers = "1 2 4 8 16 32 64";
+      maxtime = "10000h"; # Do not ban for more than 1 week
+      overalljails = true; # Calculate the bantime based on all the violations
+    };
+    jails = {
+      apache-nohome-iptables.settings = {
+        # Block an IP address if it accesses a non-existent
+        # home directory more than 5 times in 10 minutes,
+        # since that indicates that it's scanning.
+        filter = "apache-nohome";
+        action = ''iptables-multiport[name=HTTP, port="http,https"]'';
+        logpath = "/var/log/httpd/error_log*";
+        backend = "auto";
+        findtime = 600;
+        bantime = 600;
+        maxretry = 5;
+      };
+    };
+  };
 
   # Open ports in the firewall.
-  networking.firewall.allowedTCPPorts = [22 80 443 5432 9090 8080];
-  networking.firewall.allowedUDPPorts = [22 80 443 5432 9090 8080];
+  networking.firewall.allowedTCPPorts = [22 80 443 5432 8080];
+  networking.firewall.allowedUDPPorts = [22 80 443 5432 8080];
   # Or disable the firewall altogether.
   networking.firewall.enable = true;
 
