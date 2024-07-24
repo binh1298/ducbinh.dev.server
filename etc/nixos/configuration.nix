3e0bb09f3d4db152c0a7e16dc53fb0565563976b
Pham Duc Binh <binh1298@gmail.com>
2024-07-25 00:20:44 +0700

    add lazygit and fix fail2ban

--- etc/nixos/configuration.nix
+++ etc/nixos/configuration.nix
@@ -83,6 +83,7 @@
     docker-compose
     tmux
     btop
+    lazygit
   ];
 
   virtualisation.docker.enable = true;
@@ -112,7 +113,6 @@
     bantime = "24h"; # Ban IPs for one day on the first ban
     bantime-increment = {
       enable = true; # Enable increment of bantime after each violation
-      formula = "ban.Time * math.exp(float(ban.Count+1)*banFactor)/math.exp(1*banFactor)";
       multipliers = "1 2 4 8 16 32 64";
       maxtime = "10000h"; # Do not ban for more than 1 week
       overalljails = true; # Calculate the bantime based on all the violations
